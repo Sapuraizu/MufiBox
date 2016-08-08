5f9e70525a49ec882c2682efe0c8cb5a032498b1
Sapuraizu <sapuraizu.off@gmail.com>
2016-08-09 00:43:06 +0200

    Update #3
    Ajout de la date dans les messages, suppression de ses propres messages
    disponible, fixation de l'envois de commandes et correction de bugs
    mineurs.

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 	Crée par Sapuraizu par pur ennuie,
 	vous pouvez réutiliser ce "projet" à titre personnel (Je vois pas ce que vous pourrez en faire autrement de toute manière.),
 -->
-<?php
-	$uid = (isset($_GET['uid']) && !empty($_GET['uid'])) ? $_GET['uid'] : "";
-	$token = (isset($_GET['token']) && !empty($_GET['token'])) ? $_GET['token'] : "";
-?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -30,10 +26,9 @@
 				<table id="historic" class="table table-hover table-condensed">
 					<thead>
 						<tr>
-							<th>#</th>
-							<th>User ID</th>
+							<!--<th>#</th>-->
 							<th>Username</th>
-							<th>Message</th>
+							<th colspan="2">Message</th>
 							<th>Date d'envois</th>
 						</tr>
 					</thead>
@@ -47,42 +42,48 @@
 		<script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
 		<script type="text/javascript" src="js/bootstrap/bootstrap.min.js"></script>
 		<script type="text/javascript" src="js/javascript.js"></script>
+		<script type="text/javascript" src="js/moments.js"></script>
 
 		<!-- Socket IO -->
+		<?php
+			$uid = (isset($_GET['uid']) && !empty($_GET['uid'])) ? $_GET['uid'] : "";
+			$token = (isset($_GET['token']) && !empty($_GET['token'])) ? $_GET['token'] : "";
+			echo '<script>var uid = "'. $uid .'", token = "'. $token .'";</script>';
+		?>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.8/socket.io.min.js"></script>
 		<script>
-			connectToMufiShout("<?php echo $uid; ?>", "<?php echo $token; ?>", "http://shoutbox.mufibot.net:8080/");
+			connectToMufiShout(uid, token, "http://shoutbox.mufibot.net:8080/");
 			function connectToMufiShout(uid, token, url){
 				var shoutbox = io.connect(url),
 					isWritingMultipleLines = false;
 
 				shoutbox.on('connect', function(){
-					console.log('%cConnected.', 'color: #16a085');
+					console.log('%cConnecté au serveur MufiBot.', 'color: #16a085');
 				})
 
 				shoutbox.on('disconnect', function(){
-					console.log('%cDisconnected.', 'color: #e74c3c');
+					console.log('%cDéconnecté du serveur MufiBot.', 'color: #e74c3c');
 				})
 
 				shoutbox.on('authentication required', function(){
-					console.log('%cAuthentification required.', 'color: #e67e22');
+					console.log('%cConnexion requise.', 'color: #e67e22');
 					$('#state').html('<span class="text-orange">Authentification...</span>');
 					shoutbox.emit('authenticate', {user_id: uid, token: token});
 				})
 
 				shoutbox.on('authentication success', function(data){
-					console.log('%cAuthentification success.', 'color: #16a085');
+					console.log('%cConnexion effecutée.', 'color: #16a085');
 					$('#state').html('<span class="text-green">Connecté.</span>');
 					shoutbox.emit('get messages', {count: 100});
 				})
 
 				shoutbox.on('authentication failure', function(data){
 					$('#state').html('<span class="text-red">Vous n\'êtes pas connecté.</span>').add;
-					console.log('%cAuthentification failure.', 'color: #e74c3c');
+					console.log('%cEchec de la connexion.', 'color: #e74c3c');
 				})
 
 				shoutbox.on('not authenticated', function(data){
-					console.log('%cNot authentificated.', 'color: #e74c3c');
+					console.log('%cVous n\'êtes pas connecté.', 'color: #e74c3c');
 				})
 
 				shoutbox.on('messages list', function(data){
@@ -98,6 +99,10 @@
 					showMessage(data);
 				})
 
+				shoutbox.on('delete message', function(data){
+					$('tr[data-mid="'+ data.id +'"] td[data-id="content"]').prepend('<span class="text-red">(Supprimé)</span> ');
+				})
+
 				shoutbox.on('popup', function(data){
 					alert(data.message);
 				})
@@ -105,12 +110,15 @@
 				function showMessage(data){
 					var result = "",
 						prefix = (data.deleted) ? "<span class=\"text-red\">(Supprimé)</span> " : "" || (data.edited) ? "<span class=\"text-red\">(Édité)</span> " : "";
-					result += '<tr class="animated fadeIn">';
-					result += '<td>'+ data.id +'</td>';
-					result += '<td>'+ data.user_id +'</td>';
-					result += '<td>'+ (data.username_link || '<a target="_blank" href="http://forum.mufibot.net/user-8385.html">[BOT] Stéphanie</a>') +'</td>';
-					result += '<td>'+ prefix + data.message +'</td>';
-					result += '<td>'+ data.timestamp +'</td>';
+					result += '<tr class="animated fadeIn" data-mid="'+ data.id +'">';
+					//result += '<td>'+ data.id +'</td>';
+					result += '<td><span data-type="talk-to" data-uid="'+ data.user_id +'">@</span>'+ (data.username_link || '<a target="_blank" href="http://forum.mufibot.net/user-8385.html">[BOT] Stéphanie</a>') +'</td>';
+					result += '<td data-id="content">'+ prefix + data.message +'</td>';
+					if(data.user_id == uid)
+						result += '<td class="text-right"><i data-type="msg_delete" class="action-icon fa fa-trash"></i> <i data-type="msg_edit" class="action-icon fa fa-pencil"></i></td>';
+					else
+						result += '<td></td>';
+					result += '<td class="text-right">'+ moment.unix(data.timestamp).format('HH:mm:ss') +'</td>';
 					result += '</tr>';
 					$('#historic tbody').prepend(result);
 				}
@@ -128,7 +136,7 @@
 				$(document).on('keyup', '#snd_msg', function(e){
 					if(!isWritingMultipleLines && e.keyCode == 13){
 						var _this = $(this),
-							message = _this.val();
+							message = _this.val().trim();
 						_this.val('');
 						shoutbox.emit('new message', {message: message});
 					} else if(e.keyCode == 16) {
@@ -139,6 +147,21 @@
 				$(document).on('click', '[data-type="snd_cmd"]', function(){
 					var command = $(this).attr('data-command');
 					shoutbox.emit('new message', {message: command});
+				})
+
+				$(document).on('click', '[data-type="talk-to"]', function(){
+					var _this = $(this),
+						uid = _this.attr('data-uid'),
+						username = _this.next().text().trim(),
+						userprofile = _this.next().attr('href'),
+						talkTo = '[url='+ userprofile +']@'+username+'[/url] ';
+					$('#snd_msg').val(talkTo).focus();
+				})
+
+				$(document).on('click', '[data-type="msg_delete"]', function(){
+					var _this = $(this),
+						mid = parseInt(_this.closest('tr').attr('data-mid'));
+					shoutbox.emit('delete message', {id: mid});
 				})
 			}	
 		</script>
